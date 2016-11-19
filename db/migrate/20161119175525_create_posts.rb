2c89523b051ccc54f4666f37bcb05ddb883be307
Evan Shabsove <evan.shabsove@gmail.com>
2016-11-19 15:37:54 -0500

    migrated my dad into schema, and took off user_id from posts b/c it was not nessisary

--- db/migrate/20161119175525_create_posts.rb
+++ db/migrate/20161119175525_create_posts.rb
@@ -4,7 +4,6 @@ class CreatePosts < ActiveRecord::Migration[5.0]
       t.string :post_title
       t.string :post_content
       t.integer :blog_id
-      t.integer :user_id
       t.timestamps
     end
   end
