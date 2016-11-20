05a611ab33edbf017033d371916ede8c787c552e
Evan Shabsove <evan.shabsove@gmail.com>
2016-11-20 13:22:47 -0500

    Made log-in feature to website

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in!"
+    else
+      render "new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to home_url, notice: "Logged out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
@@ -14,5 +9,7 @@ Rails.application.routes.draw do
     resources :posts
 
   end
+
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
