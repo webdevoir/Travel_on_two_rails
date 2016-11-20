ffbc24bb0f67005a6c46c744ac6ca17fea5cdf3c
Evan Shabsove <evan.shabsove@gmail.com>
2016-11-20 12:07:29 -0500

    added a form to make a blog, not working right now because we need a session to store the user_id

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,32 @@
 class BlogsController < ApplicationController
+
+  def index
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to blogs_url
+    else
+      render :new
+    end
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+
+
+  private
+  def blog_params
+    params.require(:blog).permit(:blog_name)
+  end
+
 end
