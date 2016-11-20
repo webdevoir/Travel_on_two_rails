16bdcf3e6011d354b67acc86e44e13ddbbec2069
Evan Shabsove <evan.shabsove@gmail.com>
2016-11-20 15:05:50 -0500

    added form for post controller and made new and create on post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to blogs_url
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:post_title, :post_content)
+  end
 end
