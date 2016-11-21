c0337d38ef744c233207acaf8ed72d856033f107
Evan Shabsove <evan.shabsove@gmail.com>
2016-11-20 21:50:34 -0500

    got submiting posts to work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
+  before_action :load_blog
+
 
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = @blog.posts.build(post_params)
 
     if @post.save
       redirect_to blogs_url
@@ -17,5 +19,9 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:post_title, :post_content)
+  end
+
+  def load_blog
+    @blog = Blog.find(params[:blog_id])
   end
 end
