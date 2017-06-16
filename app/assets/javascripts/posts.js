860c87c44b4e97f3a7e9bdbe325a6e7443c8d562
Evan Shabsove <evan.shabsove@gmail.com>
2017-06-15 20:21:39 -0400

    uploader is uploading the image, just cant display them for whatever reason but good enough for now

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
     $("#post-file-container").append(
       "<div class='field-title'>" +
         "<label for='post_post_pictures_attributes_" + count_string + "_picture'>Upload an Image here</label>" +
-        "<input class='form-control' type='file' name='post[post_pictures_attributes]["+ count_string +"][picture]' id='post_post_pictures_attributes_" + count_string + "_picture'>" +
+        "<input class='form-control' type='file' name='post_pictures[image][]' id='post_post_pictures_attributes_" + count_string + "_picture'>" +
       "</div>"
     )
   });
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,39 +5,45 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     1.times { @post.post_pictures.build}
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @article }
+    end
   end
 
   def create
     @post = @trip.posts.build(post_params)
 
-    params[:post][:post_pictures_attributes].each do |number|
-      @post_picture = PostPicture.new
-      @post_picture.picture = params[:post][:post_pictures_attributes][number][:picture]
-      raise 'hit'
-      if @post_picture.save
-        raise 'hit'
-        next
-      else
-        raise 'hit'
-        raise[:error]
-      end
-    end
-
     if @post.save
+      params[:post_pictures]['image'].each do |img|
+        @post_picture = @post.post_pictures.create!(:picture => img)
+      end
       redirect_to trip_path(@trip), notice: "Your post was posted!"
     else
+      raise[:error]
       render :new
     end
+
   end
 
   def show
     @post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @article }
+    end
   end
 
   private
   def post_params
     params.require(:post).permit(:post_title, :post_content, :address1, :address2)
   end
+
+  def post_picture_params
+    params.require("0").permit(:picture)
+  end
+
 
   def load_trip
     @trip = Trip.find(params[:trip_id])
