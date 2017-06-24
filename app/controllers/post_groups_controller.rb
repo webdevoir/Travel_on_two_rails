14c1b1c9143fa4e94f1e44915f42bf702f70473b
Evan Shabsove <evan.shabsove@gmail.com>
2017-06-24 17:02:36 -0400

    added post group box and are able to upload images to them

--- app/controllers/post_groups_controller.rb
+++ app/controllers/post_groups_controller.rb
@@ -8,16 +8,30 @@ class PostGroupsController < ApplicationController
     @user = @trip.user
   end
 
+  def update
+    @post_group = PostGroup.find(params[:id])
+    if @post_group.update_attributes(post_group_params)
+      redirect_to trip_path(@trip)
+    else
+      flash[:error] = "Something went wrong"
+      redirect_to trip_path(@trip)
+    end
+  end
+
   def fetch_post
     @post = Post.find(params[:id])
     @post_pictures = @post.post_pictures
     respond_to do |format|
-      format.js 
+      format.js
     end
   end
 
   private
   def load_trip
     @trip = Trip.find(params[:trip_id])
+  end
+
+  def post_group_params
+    params.require(:post_group).permit(:month, :year, :image)
   end
 end
