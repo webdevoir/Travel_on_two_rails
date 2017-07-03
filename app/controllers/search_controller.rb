06d05e6ab37779a7d4e2706f15ce381af7f74bbd
Evan Shabsove <evan.shabsove@gmail.com>
2017-07-03 19:23:57 -0400

    made it so it only returns one instance of a user after a search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,29 @@ class SearchController < ApplicationController
     if params[:q].nil?
       @trips = []
     else
-      @trips = Elasticsearch::Model.search(params[:q], [Trip, User]).records.to_a
+      @trips_and_users = Elasticsearch::Model.search(params[:q], [Trip, User]).records.to_a
+      @users = seperate_trips_and_users(@trips_and_users)
     end
+  end
+
+
+  private
+
+  def seperate_trips_and_users(trips_and_users)
+    users = []
+    trips_and_users.each do |trip_or_user|
+      if trip_or_user.class.name == "Trip"
+        # for whatever reason include? was not working here so this was next best thing
+        unless users.any? {|h| h.id == trip_or_user.user.id }
+          users << trip_or_user.user
+        end
+      else
+        unless users.include? trip_or_user
+          users << trip_or_user
+        end
+      end
+    end
+    return users
   end
 
 end
