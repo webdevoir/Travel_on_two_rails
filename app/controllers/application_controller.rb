0f4f4efee6c6dbc15c36d124c8e7bd9a87ecb74b
Evan Shabsove <evan.shabsove@gmail.com>
2017-07-03 19:36:27 -0400

    fixed a few bugs on users show if no data, should ask for more details on sign up (location, last name)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user)
+  end
+
 end
