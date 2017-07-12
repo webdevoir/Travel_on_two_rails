6e46aad0b62fe965debafe6dfd0284dd789d7d23
Evan Shabsove <evan.shabsove@gmail.com>
2017-07-11 20:10:04 -0400

    created validation that demands uniqueness of both values

--- app/models/followed_blog.rb
+++ app/models/followed_blog.rb
@@ -1,4 +1,6 @@
 class FollowedBlog < ApplicationRecord
   belongs_to :user
   belongs_to :blog_owner, :foreign_key => :blog_owner_id, class_name: 'User'
+
+  validates_uniqueness_of :user_id, :scope => :blog_owner_id
 end
