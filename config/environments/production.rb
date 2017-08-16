3ec14b011b9fa7e4b40564afa65a978b24ee138a
Evan Shabsove <evan.shabsove@gmail.com>
2017-08-15 23:14:35 -0400

    configured carrier wave to hopefully use s3 in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,16 @@ Rails.application.configure do
   config.assets.compile = false
   config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    }
+    config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  end
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
