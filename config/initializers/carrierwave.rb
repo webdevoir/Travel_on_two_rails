a2a301cadcd350c83ef72c3018377f824a8d8761
Evan Shabsove <evan.shabsove@gmail.com>
2017-11-27 21:54:18 -0500

    carrier wave to include region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ unless Rails.env.development?
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
       aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+      :region                 => 'us-east-1'
     }
     config.fog_directory  = ENV["AWS_BUCKET"]              # required
   end
