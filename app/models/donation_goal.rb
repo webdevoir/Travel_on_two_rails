a5a2f99a2f3f76a4713daaeabdc5fc4de55b0eb1
Evan Shabsove <evan.shabsove@gmail.com>
2017-12-22 11:05:58 -0500

    added validations to donation goal and posts

--- app/models/donation_goal.rb
+++ app/models/donation_goal.rb
@@ -1,5 +1,9 @@
 class DonationGoal < ApplicationRecord
+  include ActiveModel::Validations
+
   belongs_to :trip
   has_many :purchases
   has_many :buyers, through: :purchases
+
+  validates :amount, :title, :end_date, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
+  include ActiveModel::Validations
+
   belongs_to :post_group
   belongs_to :trip
   has_many :post_pictures
 
   accepts_nested_attributes_for :post_pictures, allow_destroy: true
+  validates :post_title, :post_content, :day, presence: true
 end
