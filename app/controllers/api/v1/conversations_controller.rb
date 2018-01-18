0125d5e4c47039504661415217213ac48cfb4f30
Evan Shabsove <evan.shabsove@gmail.com>
2018-01-18 00:46:33 -0500

    did a small thing

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -17,10 +17,12 @@ class Api::V1::ConversationsController < Api::V1::BaseController
 
   api :get, "conversations/:id"
   def show
-    conversation = Conversation.find(params[:id])
-    result = ConversationSerializer.new(conversation)
 
-    render(json: result.to_json)
+    response = { "success" => true, "conversation" => [] }
+    conversation = Conversation.find(params[:id])
+    response["conversation"] = ConversationSerializer.new(conversation)
+
+    render(json: response.to_json)
   end
 
   api :post, "conversations"
