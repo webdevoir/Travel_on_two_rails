e2eb3d8f8b69d6d51ed0163a02ef3f1e9b0a784b
Evan Shabsove <evan.shabsove@gmail.com>
2018-01-19 00:39:20 -0500

    updated followed blog response

--- app/controllers/api/v1/followed_blogs_controller.rb
+++ app/controllers/api/v1/followed_blogs_controller.rb
@@ -12,16 +12,17 @@ class Api::V1::FollowedBlogsController < Api::V1::BaseController
       results << result
     end
 
-    render(json: results.to_json)
+    render(json: { success: true, followed_blogs: results }.to_json)
   end
 
   api :post, "users/:user_id/followed_blogs"
+  param :current_user_id, Integer, :desc => "Current User Id"
   def create
-    current_user = User.find(1)
+    current_user = User.find(params[:current_user_id])
     @user = User.find(params[:user_id])
     @followed_blog = FollowedBlog.new(blog_owner_id: @user.id, user_id: current_user.id)
     if @followed_blog.save
-      render(json: {:success => "success"}.to_json)
+      render(json: {:success => true}.to_json)
     else
       render(json: {:success => "error"}.to_json)
     end
