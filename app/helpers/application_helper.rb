acc85dbbffc3ded005ad3c5720ec931f5b768ee5
Evan Shabsove <evan.shabsove@gmail.com>
2018-03-19 21:17:33 -0400

    poi now on drop down and different icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{ENV['STRIPE_CLIENT_ID']}&scope=read_write"
   end
 
+  def poi_categories
+    [
+      ["POI", "poi"],
+      ["Caution", "caution"],
+      ["Lodging", "lodging"],
+      ["Groceries", "groceries"],
+      ["WIFI", "wifi"],
+      ["Bike Shop", "bike shop"]
+    ]
+  end
+
 end
