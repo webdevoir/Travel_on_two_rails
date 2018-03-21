94c72f62086a77c1f5a833eafe762e807592d302
Evan Shabsove <evan.shabsove@gmail.com>
2018-03-20 21:10:15 -0400

    can now save route with address and not lat long stuff

--- app/controllers/point_of_interests_controller.rb
+++ app/controllers/point_of_interests_controller.rb
@@ -3,6 +3,8 @@ class PointOfInterestsController < ApplicationController
   def create
     @route = Route.find(params[:route_id])
     @point_of_interest = PointOfInterest.new(point_of_interest_params)
+    @point_of_interest.latitude = params[:latitude]
+    @point_of_interest.longitude = params[:longitude]
     @point_of_interest.route_id = @route.id
     if @point_of_interest.save
       flash[:success] = "Saved POI"
@@ -19,6 +21,6 @@ class PointOfInterestsController < ApplicationController
 
   private
   def point_of_interest_params
-    params.require(:point_of_interest).permit(:latitude, :longitude, :type, :category, :info)
+    params.require(:point_of_interest).permit(:type, :category, :info)
   end
 end
